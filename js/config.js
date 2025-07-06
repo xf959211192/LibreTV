d6870915785cc603c59e68f9f87c68ccd0f46c97
晓风 <82152851+xf959211192@users.noreply.github.com>
2025-07-06 17:22:20 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -124,43 +124,43 @@ const API_SITES = {
     // 而作为观众无辜吗？毫无疑问，他们促成了黄色产业链的再生产。
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
     ckzy: {
-        api: 'https://www.ckzy1.com'，
+        api: 'https://www.ckzy1.com',
         name: 'CK资源',
         adult: true
     },
     jkun: {
-        api: 'https://jkunzyapi.com'，
+        api: 'https://jkunzyapi.com',
         name: 'jkun资源',
         adult: true
     },
     bwzy: {
-        api: 'https://api.bwzym3u8.com'，
+        api: 'https://api.bwzym3u8.com',
         name: '百万资源',
         adult: true
     },
     souav: {
-        api: 'https://api.souavzy.vip'，
+        api: 'https://api.souavzy.vip',
         name: 'souav资源',
         adult: true
     },
     r155: {
-        api: 'https://155api.com'，
+        api: 'https://155api.com',
         name: '155资源',
         adult: true
     },
     lsb: {
-        api: 'https://apilsbzy1.com'，
+        api: 'https://apilsbzy1.com',
         name: 'lsb资源',
         adult: true
     },
     huangcang: {
-        api: 'https://hsckzy.vip'，
+        api: 'https://hsckzy.vip',
         name: '黄色仓库',
         adult: true,
         detail: 'https://hsckzy.vip'
     },
     yutu: {
-        api: 'https://yutuzy10.com'，
+        api: 'https://yutuzy10.com',
         name: '玉兔资源',
         adult: true
     },
